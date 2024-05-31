5887ef5c2b341c0c9a8547516c3965e3cb8f9a13
Zerih55 <168442963+Zerih55@users.noreply.github.com>
2024-05-31 12:43:27 +0800

    Add files via upload

---
+++ JSbasic/app.js
@@ -0,0 +1,120 @@
+//This is a single line//
+
+// let -changeable Variable 
+// const - Constant Value
+// var - olderway of declaring variables
+
+//Variables
+
+let firstName="John";
+console.log(firstName);
+
+const my_age = 18;
+console.log(my_age);
+
+firstName = "Ralph";
+console.log(firstName);
+
+//Boolean = true or false
+const myDice = true;
+console.log(myDice);
+console.log("Is myDice ", myDice);
+
+//Primitive Data Types
+//Numbers
+// String
+// boolean
+
+//undefined
+let myCity;
+console.log(myCity);
+
+//null
+let myCar = null;
+console.log(null);
+
+//Object Types
+//Objects
+//Arrays
+//Function
+
+//JS Operators
+//Addition (+)
+//Subtraction (-)
+//Multiplication (*)
+//Division (/)
+//Modulus (%)
+
+let x = 5;
+let y = 2;
+
+let sum = x+y; //Addition
+console.log(sum)
+
+// Difference
+
+let diff=x-y;
+console.log(diff);
+
+// product
+let product = x*y;
+console.log(product)
+
+// Division
+let quotient=x/y;
+console.log(quotient)
+
+// remainder - modulus
+let remainder= x%y;
+console.log(remainder);
+
+//Logical Expression
+// && (AND) , || (OR) , ! (NOT)
+let sunny = true; //1
+let warm = false; //0
+// ADD multiply
+// OR add
+//NOT !
+
+console.log("sunny and warm", sunny && warm);
+console.log("sunny or warm", sunny || warm);
+console.log("!Sunny", !sunny);
+//Assignment Operators
+// += -= *= /=
+let a = 10;
+let b = 5;
+a += b;  // shorthand for a = a+b
+console.log(a);
+a += a; //short a= a+a
+console.log(a);
+a -=b; //short a = a -b
+console.log(a);
+a *= b;  //a = a * b
+console.log(a);
+a /=b;
+console.log(a)
+
+// Function
+// call function and pass argument to them
+
+function myFunction(num1=4, num2=6){
+    console.log(num1+num2);
+}
+myFunction(); //4+6= 10 
+
+getProdcut (10,20);
+
+funtion areaRectangle(length,width){
+    //length x width
+    console.log(length*width)
+}
+
+areaRectangle(5,4);
+
+// Control Structions
+// conditionals
+// if statement
+
+
+
+    
